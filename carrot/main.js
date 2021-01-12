d7cbc1d07bef2adb32c54668e93d619b89e12a87
dhchun1203 <dhchun1203@gmail.com>
2021-01-12 12:44:00 +0900

    Carrot Game: javascript + CSS final touch

--- carrot/main.js
+++ carrot/main.js
@@ -76,6 +76,7 @@ function showStopButton() {
 	const icon = gameBtn.querySelector(".fas");
 	icon.classList.add("fa-stop");
 	icon.classList.remove("fa-play");
+	gameBtn.style.visibility = "visible";
 }
 
 function hideGameButton() {
